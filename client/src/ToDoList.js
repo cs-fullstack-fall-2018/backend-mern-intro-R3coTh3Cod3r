99a7361866320b421a3f49fe01127aff24a1d225
Mareco Nabors <naborsmarico99@gmail.com>
2018-09-17 12:45:37 -0500

    tried

--- client/src/ToDoList.js
+++ client/src/ToDoList.js
@@ -1,75 +1,63 @@
 import React, {Component} from 'react';
 
-var forAnotherFunction = this.props.arr.map(
-    eachItem => {
-        return(
-            <div>
-                <p>
-
-                    {eachItem._id} {eachItem.username } has to { eachItem.todo} {eachItem.isDone}
-                </p>
-                <button onClick={()=>this.props.delete(eachItem._id)}>Delete</button>
-                <hr/>
-                <h2>{this.props.arr.todo}</h2>
-                <h2>{forAnotherFunction}</h2>
-            </div>
-        )
-    }
-
-
-
-);
 
 class ToDoList extends Component {
+
+
+
+    var forEachItem = this.props.arr.map(
+    eachItem => {
+        //console.log(eachItem.isDone)
+        if(eachItem.isDone === true){
+            (eachItem.isDone) = "is Done"}
+        else if(eachItem.isDone ===false)
+            (eachItem.isDone) = "is not Done"
+
 
 
     render() {
         //console.log("Array")
         //console.log(this.props.arr)
 
-        var forEachItem = this.props.arr.map(
-            eachItem => {
-                //console.log(eachItem.isDone)
-                if (eachItem.isDone === true) {
-                    (eachItem.isDone) = "is Done"
-                }
-                else if (eachItem.isDone === false)
-                    (eachItem.isDone) = "is not Done"
 
 
-                return (
+                return(
                     <div>
                         <p>
 
-                            {eachItem._id} {eachItem.username} has to {eachItem.todo} {eachItem.isDone}
+                            {eachItem._id} {eachItem.username } has to { eachItem.todo} {eachItem.isDone}
                         </p>
-                        <button onClick={() => this.props.deleteFunction(eachItem._id)}>Delete</button>
+                        <button onClick={()=>this.props.deleteFunction(eachItem._id)}>Delete</button>
                         <hr/>
                     </div>
 
                 )
 
             }
+
+
         );
-        //
-        //     return (
-        //
-        //         <div>
-        //
-        //             <h2>{this.props.arr.todo}</h2>
-        //             <h2> {forEachItem}</h2>
-        //
-        //         </div>
-        //
-        //
-        //     );
-        //
-        //
-        // }
-        //
-        //
-
-
-    }
 }
-export default ToDoList;
+
+        return (
+
+            <div>
+
+                <h2>{this.props.arr.todo}</h2>
+                <h2> {forEachItem}</h2>
+
+            </div>
+        )
+
+
+
+
+
+
+
+
+
+
+
+}
+export default ToDoList
